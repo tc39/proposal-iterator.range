3efcd46d065acd1df11365fc96cd7efaf38f31a6
Jack Works <jackworks@protonmail.com>
2020-02-15 19:33:58 +0800

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -4,7 +4,7 @@ interface NumberConstructor {
     range(to: number): Iterator<number>
 }
 interface BigIntConstructor {
-    range(from: BigInt, to: BigInt, step?: BigInt): Iterator<BigInt>
+    range(from: bigint, to: bigint, step?: bigint): Iterator<bigint>
     // If accept BigInt.range(to)
-    range(to: BigInt): Iterator<BigInt>
+    range(to: bigint): Iterator<bigint>
 }
