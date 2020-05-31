422e1d360105dbe6230624b777b38d62556c18b2
Jack Works <jackworks@protonmail.com>
2020-05-31 22:53:02 +0800

    support smart step

--- polyfill.js
+++ polyfill.js
@@ -1,51 +1,52 @@
+// @ts-check
 /// <reference path="./global.d.ts" />
 // syntax target = es2020
 // This polyfill requires: globalThis, BigInt & private fields
-;(() => {
+{
     /*
      * Behaviour flags
      * This proposal is in early stage.
      * Use which in the end depends on community discussion.
      */
     /**
-     * @type{boolean}
+     * @type {boolean}
      * This flag treat `range(to)` as `range(0, to)`
      */
     const isAcceptAlias = false
+    /** @template {number | bigint} T */
     class RangeIterator {
         /**
-         *
-         * @param {number | bigint} from
-         * @param {number | bigint | undefined} to
-         * @param {number | bigint | undefined | null | object} option
+         * @param {T} from
+         * @param {T | number | undefined} to
+         * @param {T | undefined | null | { step?: T }} option
          * @param {"number" | "bigint"} type
          */
         constructor(from, to, option, type) {
-            // Step 1 to 6
             if (typeof from !== type) throw new TypeError()
-            if (type !== "number" && type !== "bigint") throw new TypeError()
-            const zero = type === "bigint" ? 0n : 0
-            const one = type === "bigint" ? 1n : 1
+            if (type !== "number" && type !== "bigint") throw new TypeError() // @ts-ignore
+            /** @type {T} */ const zero = type === "bigint" ? 0n : 0 // @ts-ignore
+            /** @type {T} */ const one = type === "bigint" ? 1n : 1
             if (isAcceptAlias === false) {
             } else if (typeof to === "undefined") {
                 // range(to) equals to range(zero, to)
                 to = from
                 from = zero
             }
-            /** @type {number | bigint} */
-            let step
-            if (typeof option === "undefined" || option === null) step = one
-            else if (typeof option === "object") step = option.step
-            else if (typeof option === type) step = option
-            else throw new TypeError()
-            if (typeof step === "undefined") step = one
             if (typeof from !== type) throw new TypeError()
-            // Step 7 to 11
             // Allowing all kinds of number (number, bigint, decimals, ...) to range from a finite number to infinity.
             if (!isInfinity(to) && typeof to !== type) throw new TypeError()
-            if (isInfinity(from) || isInfinity(step)) throw RangeError()
+            if (isInfinity(from)) throw RangeError()
+            const ifIncrease = to > from
+            /** @type {T} */ let step
+            if (typeof option === "undefined" || option === null) {
+                if (ifIncrease) step = one
+                // @ts-ignore
+                else step = -one
+            } else if (typeof option === "object") step = option.step
+            else if (typeof option === type) step = option
+            else throw new TypeError()
+            if (isInfinity(step)) throw RangeError()
             if (step === zero && from !== to) throw new RangeError()
-            // Step 13 - 18
             this.#from = from
             this.#to = to
             this.#step = step
@@ -54,39 +55,32 @@
             return this
         }
         //#region internal slots
-        /** @type {number | bigint} */
-        #from
-        /** @type {number | bigint} */
-        #to
-        /** @type {number | bigint} */
-        #step
-        /** @type {"number" | "bigint"} */
-        #type
-        /** @type {number | bigint} */
-        #currentCount
+        /** @type {T} */ #from
+        /** @type {number | bigint} */ #to
+        /** @type {T} */ #step
+        /** @type {"number" | "bigint"} */ #type
+        /** @type {T} */ #currentCount
         //#endregion
         /**
-         * @returns {IteratorResult<number | bigint>}
+         * @returns {IteratorResult<T>}
          */
         next() {
-            // Step 1 to 3 omitted. Private field will do the brand check
             const from = this.#from
             const to = this.#to
             const step = this.#step
             const type = this.#type
             if (type !== "bigint" && type !== "number") throw new TypeError()
-            if (from === to) return CreateIterResultObject(undefined, true)
-            const zero = type === "bigint" ? 0n : 0
-            const one = type === "bigint" ? 1n : 1
+            if (from === to) return CreateIterResultObject(undefined, true) // @ts-ignore
+            /** @type {T} */ const zero = type === "bigint" ? 0n : 0 // @ts-ignore
+            /** @type {T} */ const one = type === "bigint" ? 1n : 1
             if (Number.isNaN(from) || Number.isNaN(to) || Number.isNaN(step))
                 return CreateIterResultObject(undefined, true)
             const ifIncrease = to > from
             const ifStepIncrease = step > zero
             if (ifIncrease !== ifStepIncrease)
                 return CreateIterResultObject(undefined, true)
-            // Step 19
-            const currentCount = this.#currentCount
-            const currentYieldingValue = from + step * currentCount
+            const currentCount = this.#currentCount // @ts-ignore
+            const currentYieldingValue = from + step * currentCount // @ts-ignore
             const nextCount = currentCount + one
             if (ifIncrease && currentYieldingValue >= to)
                 return CreateIterResultObject(undefined, true)
@@ -117,15 +111,15 @@
     })
     Object.defineProperty(Number, "range", {
         configurable: true,
+        writable: true,
         value: (from, to, option) =>
             new RangeIterator(from, to, option, "number"),
-        writable: true,
     })
     Object.defineProperty(BigInt, "range", {
         configurable: true,
+        writable: true,
         value: (from, to, option) =>
             new RangeIterator(from, to, option, "bigint"),
-        writable: true,
     })
     function isInfinity(x) {
         if (typeof x !== "number") return false
@@ -136,4 +130,4 @@
     function CreateIterResultObject(value, done) {
         return { value, done }
     }
-})()
+}
