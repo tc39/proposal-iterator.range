9afc08db1cfbba91161cf94f27169399ced23061
Jack Works <jackworks@protonmail.com>
2020-06-12 13:39:53 +0800

    update .d.ts

--- global.d.ts
+++ global.d.ts
@@ -5,12 +5,17 @@ interface RangeIterator<T extends number | bigint>
     [Symbol.iterator](): RangeIterator<T>
     readonly [Symbol.toStringTag]: "RangeIterator"
     readonly start: T
-    readonly to: T | Infinity
+    readonly end: T | Infinity
     readonly step: T
+    readonly inclusive: boolean
 }
 type RangeFunction<T extends number | bigint> = {
-    range(start: T, to: T | Infinity, option?: { step?: T }): RangeIterator<T>
-    range(start: T, to: T | Infinity, step?: T): RangeIterator<T>
+    range(
+        start: T,
+        end: T | Infinity,
+        option?: T | { step?: T; inclusive?: boolean }
+    ): RangeIterator<T>
+    range(start: T, end: T | Infinity, step?: T): RangeIterator<T>
 }
 interface NumberConstructor extends RangeFunction<number> {}
 interface BigIntConstructor extends RangeFunction<bigint> {}
