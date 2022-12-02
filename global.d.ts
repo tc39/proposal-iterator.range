b1932a4b7f1d6f1c3b9846ea203d6cf483879af7
Jack Works <jackworks@protonmail.com>
2022-12-02 11:43:14 +0800

    fix: .d.ts

--- global.d.ts
+++ global.d.ts
@@ -2,10 +2,10 @@ type Infinity = number
 declare var Iterator: {
     new (): Iterator<never, void, void>
     prototype: Iterator<never, void, void>
-    range(start: number, end: number, option: number | NumericRangeOptions): IterableIterator<number>
-    range(start: bigint, end: bigint | Infinity, option: bigint | NumericRangeOptions): IterableIterator<bigint>
+    range(start: number, end: number, option: number | NumericRangeOptions<number>): IterableIterator<number>
+    range(start: bigint, end: bigint | Infinity, option: bigint | NumericRangeOptions<bigint>): IterableIterator<bigint>
 }
-interface NumericRangeOptions {
-    step?: number
+interface NumericRangeOptions<T extends bigint | number> {
+    step?: number | number
     inclusive?: boolean
 }
